4dfe317d504c224594cc826204dd07aba716b7ea
TbcProjects <daniel@thebikeclub.co.uk>
2023-07-02 11:33:52 +0100

    Add theme color config

--- src/theme/foundations/colors.ts
+++ src/theme/foundations/colors.ts
@@ -1,3 +1,10 @@
-const colors = {};
+const colors = {
+  cyan: "#5FB4A2",
+  darkBlue: "#203A4C",
+  greyDarkBlue: "#33323D",
+  lightGrey: "#FAFAFA",
+  mediumGrey: "#EAEAEB",
+  brightRed: "#F43030",
+};
 
 export default colors;

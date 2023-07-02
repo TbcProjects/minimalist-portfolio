d2113a14a778228aa78084d775b3e8981357d1f6
TbcProjects <daniel@thebikeclub.co.uk>
2023-07-02 22:00:03 +0100

    Import all homepage components to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
-"use client";
-
-import Link from "next/link";
-
-import { Heading, Text } from "@chakra-ui/react";
+import HomePageContainer from "@/components/containers/HomePageContainer";
+import Hero from "@/components/sections/Hero";
+import AboutMe from "@/components/sections/AboutMe";
+import ContactBanner from "@/components/sections/ContactBanner";
 
 export default function Home() {
   return (
-    <main>
-      <Text textStyle={"h1"}>Home page</Text>
-      <div>
-        <Link href="/portfolio">Click to view portfolio</Link>
-        <Link href="/contact">Contact us</Link>
-      </div>
-    </main>
+    <HomePageContainer as={"main"}>
+      <Hero />
+      <AboutMe />
+      <ContactBanner />
+    </HomePageContainer>
   );
 }

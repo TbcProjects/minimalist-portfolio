63159c27b91a4037009796e64e7f0b6d4cf55364
TbcProjects <daniel@thebikeclub.co.uk>
2023-07-06 22:02:05 +0100

    Add Homepage wrapper component

--- src/components/containers/HomePageContainer.tsx
+++ src/components/containers/HomePageContainer.tsx
@@ -1,9 +1,7 @@
 "use client";
 import { ReactNode } from "react";
 
-import Link from "next/link";
-
-import { Container, ContainerProps, Text } from "@chakra-ui/react";
+import { Container, ContainerProps } from "@chakra-ui/react";
 
 interface HomePageContainerProps extends ContainerProps {
   children?: ReactNode;
@@ -12,18 +10,5 @@ interface HomePageContainerProps extends ContainerProps {
 export default function HomePageContainer({
   children,
 }: HomePageContainerProps) {
-  return (
-    <Container maxW="container.xl">
-      <Text as={"h1"} textStyle={"h1"}>
-        Heading h1
-      </Text>
-      <Text textStyle={"h2"}>Heading h2</Text>
-      <Text textStyle={"h3"}>Heading h3</Text>
-      <div>
-        <Link href="/portfolio">Click to view portfolio</Link>
-        <Link href="/contact">Contact us</Link>
-      </div>
-      {children}
-    </Container>
-  );
+  return <Container maxW="container.xl">{children}</Container>;
 }

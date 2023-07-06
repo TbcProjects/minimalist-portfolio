56c3dfd48b684079eddab8984eb5b196beb03973
TbcProjects <daniel@thebikeclub.co.uk>
2023-07-06 19:34:59 +0100

    Add query and request test data from dato

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,31 @@
+import { gql } from "graphql-request";
+import { datoClient } from "@/lib/datoCMS/client";
+
 import HomePageContainer from "@/components/containers/HomePageContainer";
 import Hero from "@/components/sections/Hero";
 import AboutMe from "@/components/sections/AboutMe";
 import ContactBanner from "@/components/sections/ContactBanner";
 
-export default function Home() {
+const query = gql`
+  query MyQuery {
+    homepage {
+      homepageContent {
+        ... on SectionAboutMeRecord {
+          id
+          buttonLabel
+          buttonLink
+        }
+      }
+    }
+  }
+`;
+
+export default async function Home() {
+  const data = await datoClient(query);
+
   return (
     <HomePageContainer as={"main"}>
-      <Hero />
+      <Hero data={data} />
       <AboutMe />
       <ContactBanner />
     </HomePageContainer>

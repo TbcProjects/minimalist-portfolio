1ae6731e82ed87f006bd14b57f87e3fbc3b3035c
TbcProjects <daniel@thebikeclub.co.uk>
2023-07-08 14:05:45 +0100

    Add primary button styles to button config

--- src/lib/theme/components/button.ts
+++ src/lib/theme/components/button.ts
@@ -1,21 +1,19 @@
-import { cva } from "../../../../styled-system/css";
+import { cva, type RecipeVariantProps } from "../../../../styled-system/css";
 
 export const button = cva({
   base: {
-    py: "17px",
-    px: "34px",
     cursor: "pointer",
     textStyle: "button",
   },
   variants: {
     variant: {
       primary: {
-        layerStyle: "button",
-      },
-      secondary: {
-        bg: "gray.500",
-        color: "white",
+        layerStyle: "primaryButton",
+        height: "48px",
+        minW: "200px",
       },
     },
   },
 });
+
+export type ButtonVariants = RecipeVariantProps<typeof button>;

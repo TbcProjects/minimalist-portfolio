7403ce862353fbcf16271a905b819ee0912d3f6d
TbcProjects <daniel@thebikeclub.co.uk>
2023-07-10 11:43:25 +0100

    Update SiteHeader with logo and menu items

--- src/components/layout/SiteHeader.tsx
+++ src/components/layout/SiteHeader.tsx
@@ -1,3 +1,32 @@
+import Link from 'next/link';
+
+import SiteLogo from '../icons/SiteLogo';
+import DesktopMenu from '../navigation/DesktopMenu';
+import MobileMenu from '../navigation/MobileMenu';
+
+import { css } from '../../../styled-system/css';
+
+import { container, hstack } from '../../../styled-system/patterns';
+
 export default function SiteHeader() {
-  return <div>Site header</div>;
+  return (
+    <header
+      className={container({
+        position: 'relative',
+        maxW: 'breakpoint-lg',
+        px: { base: '32px', md: '38px', lg: '0' },
+      })}
+    >
+      <div className={hstack({ justify: 'space-between' })}>
+        <Link href="/">
+          <div className={css({ py: { base: '32px', md: '61px' } })}>
+            <SiteLogo />
+          </div>
+        </Link>
+
+        <DesktopMenu />
+        <MobileMenu />
+      </div>
+    </header>
+  );
 }

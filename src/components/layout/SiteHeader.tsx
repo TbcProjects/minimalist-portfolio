11018399467d0b2a7f695df264fd9ce090bc534b
TbcProjects <daniel@thebikeclub.co.uk>
2023-07-10 16:56:59 +0100

    Add variant prop to SiteLogo

--- src/components/layout/SiteHeader.tsx
+++ src/components/layout/SiteHeader.tsx
@@ -1,26 +1,26 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-import SiteLogo from '../icons/SiteLogo';
-import DesktopMenu from '../navigation/DesktopMenu';
-import MobileMenu from '../navigation/MobileMenu';
+import SiteLogo from "../icons/SiteLogo";
+import DesktopMenu from "../navigation/DesktopMenu";
+import MobileMenu from "../navigation/MobileMenu";
 
-import { css } from '../../../styled-system/css';
+import { css } from "../../../styled-system/css";
 
-import { container, hstack } from '../../../styled-system/patterns';
+import { container, hstack } from "../../../styled-system/patterns";
 
 export default function SiteHeader() {
   return (
     <header
       className={container({
-        position: 'relative',
-        maxW: 'breakpoint-lg',
-        px: { base: '32px', md: '38px', lg: '0' },
+        position: "relative",
+        maxW: "breakpoint-lg",
+        px: { base: "32px", md: "38px", lg: "0" },
       })}
     >
-      <div className={hstack({ justify: 'space-between' })}>
+      <div className={hstack({ justify: "space-between" })}>
         <Link href="/">
-          <div className={css({ py: { base: '32px', md: '61px' } })}>
-            <SiteLogo />
+          <div className={css({ py: { base: "32px", md: "61px" } })}>
+            <SiteLogo variant="dark" />
           </div>
         </Link>
 

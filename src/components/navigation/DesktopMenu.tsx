39c1bcf914bc42bc242687fb3095be9d2381e4d5
TbcProjects <daniel@thebikeclub.co.uk>
2023-07-10 22:10:34 +0100

    Update DesktopMenu with MenuLink component

--- src/components/navigation/DesktopMenu.tsx
+++ src/components/navigation/DesktopMenu.tsx
@@ -1,46 +1,28 @@
-'use client';
+"use client";
 
-import Link from 'next/link';
+import MenuLink from "./MenuLink";
 
-import { usePathname } from 'next/navigation';
+import { css } from "../../../styled-system/css";
+import { hstack } from "../../../styled-system/patterns";
 
-import { css } from '../../../styled-system/css';
-import { hstack } from '../../../styled-system/patterns';
-
-import type { MenuItem } from '@/types/general';
+import type { MenuItem } from "@/types/general";
 
 interface DeskTopMenuProps {}
 
 export default function DesktopMenu({}: DeskTopMenuProps) {
-  const path = usePathname();
-  const isPath = (itemPath: string) => path === itemPath;
-
   const menuContent: MenuItem[] = [
-    { name: 'HOME', src: '/' },
-    { name: 'PORTFOLIO', src: '/portfolio' },
-    { name: 'CONTACT', src: '/contact' },
+    { name: "HOME", src: "/" },
+    { name: "PORTFOLIO", src: "/portfolio" },
+    { name: "CONTACT", src: "/contact" },
   ];
 
-  const menuItems = menuContent.map((item, index) => {
-    return (
-      <li
-        key={index}
-        className={css({
-          textStyle: 'button',
-          color: isPath(item.src) ? 'cyan' : '',
-          _hover: {
-            opacity: isPath(item.src) ? '1' : '.5',
-          },
-        })}
-      >
-        <Link href={item.src}>{item.name}</Link>
-      </li>
-    );
+  const menuItems = menuContent.map((item) => {
+    return <MenuLink key={item.name} href={item.src} label={item.name} />;
   });
 
   return (
-    <nav className={css({ display: { base: 'none', md: 'block' } })}>
-      <ul className={hstack({ gap: '42px' })}>{menuItems}</ul>
+    <nav className={css({ display: { base: "none", md: "block" } })}>
+      <ul className={hstack({ gap: "42px" })}>{menuItems}</ul>
     </nav>
   );
 }

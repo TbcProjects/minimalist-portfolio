39b17f02b62d75860f2d2dc811c6cde12116417f
TbcProjects <daniel@thebikeclub.co.uk>
2023-07-10 22:09:36 +0100

    Update MobileMenu to use MenuLink

--- src/components/navigation/MobileMenu.tsx
+++ src/components/navigation/MobileMenu.tsx
@@ -1,47 +1,41 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
+import { useState } from "react";
 
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import MenuLink from "./MenuLink";
+import { Squash as Hamburger } from "hamburger-react";
 
-import { Squash as Hamburger } from 'hamburger-react';
+import { css } from "../../../styled-system/css";
 
-import { css } from '../../../styled-system/css';
-
-import type { MenuItem } from '@/types/general';
+import type { MenuItem } from "@/types/general";
 
 export default function MobileMenu() {
   const [isOpen, setIsOpen] = useState(false);
 
-  const path = usePathname();
-  const isPath = (itemPath: string) => path === itemPath;
-
   const menuContent: MenuItem[] = [
-    { name: 'HOME', src: '/' },
-    { name: 'PORTFOLIO', src: '/portfolio' },
-    { name: 'CONTACT ME', src: '/contact' },
+    { name: "HOME", src: "/" },
+    { name: "PORTFOLIO", src: "/portfolio" },
+    { name: "CONTACT ME", src: "/contact" },
   ];
 
   const menuItems = menuContent.map((item, index) => {
     return (
-      <li
-        key={index}
-        className={css({
-          marginBottom: '32px',
-          '&:last-child': {
-            marginBottom: '0',
+      <MenuLink
+        key={item.name}
+        href={item.src}
+        label={item.name}
+        linkStyles={{
+          marginBottom: "32px",
+          "&:last-child": {
+            marginBottom: "0",
           },
-          color: isPath(item.src) ? 'cyan' : '',
-        })}
-      >
-        <Link href={item.src}>{item.name}</Link>
-      </li>
+        }}
+      />
     );
   });
 
   return (
-    <nav className={css({ display: { md: 'none' } })}>
+    <nav className={css({ display: { md: "none" } })}>
       <div>
         <Hamburger
           label="Show menu"
@@ -51,23 +45,23 @@ export default function MobileMenu() {
         />
       </div>
       {isOpen ? (
-        <div
+        <nav
           className={css({
-            display: 'flex',
-            position: 'absolute',
-            zIndex: '99',
-            top: '90px',
-            right: '37px',
-            color: 'white',
-            minW: '223px',
-            padding: '40px 64px',
-            textAlign: 'center',
-            layerStyle: 'darkBlueOnWhite',
-            textStyle: 'button',
+            display: "flex",
+            position: "absolute",
+            zIndex: "99",
+            top: "90px",
+            right: "37px",
+            color: "white",
+            minW: "223px",
+            padding: "40px 64px",
+            textAlign: "center",
+            layerStyle: "darkBlueOnWhite",
+            textStyle: "button",
           })}
         >
           <ul>{menuItems}</ul>
-        </div>
+        </nav>
       ) : null}
     </nav>
   );

ffb09bf64751d1e9b416494eda0c3d57e7d6f12a
TbcProjects <daniel@thebikeclub.co.uk>
2023-07-10 22:12:23 +0100

    Update styled-system

--- styled-system/tokens/tokens.d.ts
+++ styled-system/tokens/tokens.d.ts
@@ -1,74 +1,19 @@
 /* eslint-disable */
-export type Token =
-  | 'colors.cyan'
-  | 'colors.pine'
-  | 'colors.yankee_blue'
-  | 'colors.japanese_indigo'
-  | 'colors.darkBlue'
-  | 'colors.greyDarkBlue'
-  | 'colors.lightGrey'
-  | 'colors.mediumGrey'
-  | 'colors.brightRed'
-  | 'fonts.heading'
-  | 'fonts.body'
-  | 'breakpoints.sm'
-  | 'breakpoints.md'
-  | 'breakpoints.lg'
-  | 'breakpoints.xl'
-  | 'breakpoints.2xl'
-  | 'sizes.breakpoint-sm'
-  | 'sizes.breakpoint-md'
-  | 'sizes.breakpoint-lg'
-  | 'sizes.breakpoint-xl'
-  | 'sizes.breakpoint-2xl';
+export type Token = "colors.cyan" | "colors.pine" | "colors.yankee_blue" | "colors.japanese_indigo" | "colors.darkBlue" | "colors.greyDarkBlue" | "colors.lightGrey" | "colors.mediumGrey" | "colors.brightRed" | "fonts.heading" | "fonts.body" | "breakpoints.sm" | "breakpoints.md" | "breakpoints.lg" | "breakpoints.xl" | "breakpoints.2xl" | "sizes.breakpoint-sm" | "sizes.breakpoint-md" | "sizes.breakpoint-lg" | "sizes.breakpoint-xl" | "sizes.breakpoint-2xl"
 
-export type ColorToken =
-  | 'cyan'
-  | 'pine'
-  | 'yankee_blue'
-  | 'japanese_indigo'
-  | 'darkBlue'
-  | 'greyDarkBlue'
-  | 'lightGrey'
-  | 'mediumGrey'
-  | 'brightRed';
+export type ColorToken = "cyan" | "pine" | "yankee_blue" | "japanese_indigo" | "darkBlue" | "greyDarkBlue" | "lightGrey" | "mediumGrey" | "brightRed"
 
-export type FontToken = 'heading' | 'body';
+export type FontToken = "heading" | "body"
 
-export type BreakpointToken = 'sm' | 'md' | 'lg' | 'xl' | '2xl';
+export type BreakpointToken = "sm" | "md" | "lg" | "xl" | "2xl"
 
-export type SizeToken =
-  | 'breakpoint-sm'
-  | 'breakpoint-md'
-  | 'breakpoint-lg'
-  | 'breakpoint-xl'
-  | 'breakpoint-2xl';
+export type SizeToken = "breakpoint-sm" | "breakpoint-md" | "breakpoint-lg" | "breakpoint-xl" | "breakpoint-2xl"
 
 export type Tokens = {
-  colors: ColorToken;
-  fonts: FontToken;
-  breakpoints: BreakpointToken;
-  sizes: SizeToken;
-} & { [token: string]: never };
+		colors: ColorToken
+		fonts: FontToken
+		breakpoints: BreakpointToken
+		sizes: SizeToken
+} & { [token: string]: never }
 
-export type TokenCategory =
-  | 'zIndex'
-  | 'opacity'
-  | 'colors'
-  | 'fonts'
-  | 'fontSizes'
-  | 'fontWeights'
-  | 'lineHeights'
-  | 'letterSpacings'
-  | 'sizes'
-  | 'shadows'
-  | 'spacing'
-  | 'radii'
-  | 'borders'
-  | 'durations'
-  | 'easings'
-  | 'animations'
-  | 'blurs'
-  | 'gradients'
-  | 'breakpoints'
-  | 'assets';
+export type TokenCategory = "zIndex" | "opacity" | "colors" | "fonts" | "fontSizes" | "fontWeights" | "lineHeights" | "letterSpacings" | "sizes" | "shadows" | "spacing" | "radii" | "borders" | "durations" | "easings" | "animations" | "blurs" | "gradients" | "breakpoints" | "assets"

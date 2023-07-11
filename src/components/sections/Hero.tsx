df5508a3240410daf9bbc72599dc74f900bbd237
TbcProjects <daniel@thebikeclub.co.uk>
2023-07-11 11:48:22 +0100

    Make data prop optional in Hero component

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,23 +1,23 @@
-import Image from 'next/image';
+import Image from "next/image";
 
-import { css } from '../../../styled-system/css';
-import { container } from '../../../styled-system/patterns';
+import { css } from "../../../styled-system/css";
+import { container } from "../../../styled-system/patterns";
 
 interface HeroProps {
-  data: any;
+  data?: any;
 }
 
 export default function Hero({ data }: HeroProps) {
   return (
     <div
       className={container({
-        maxW: 'breakpoint-lg',
-        px: { base: '32px', md: '38px', lg: '0' },
+        maxW: "breakpoint-lg",
+        px: { base: "32px", md: "38px", lg: "0" },
       })}
     >
-      <div className={css({ pos: 'relative', minH: '552px', width: '100%' })}>
+      <div className={css({ pos: "relative", minH: "552px", width: "100%" })}>
         <Image
-          src={'/images/jk-placeholder-image.jpeg'}
+          src={"/images/jk-placeholder-image.jpeg"}
           alt="placeholder image"
           objectFit="cover"
           fill={true}

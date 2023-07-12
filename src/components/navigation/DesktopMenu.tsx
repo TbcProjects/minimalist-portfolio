a79e86cb7d44006a943b6eaf8f6925b133d85573
TbcProjects <daniel@thebikeclub.co.uk>
2023-07-12 13:09:22 +0100

    Refactor desktop menu to use real data

--- src/components/navigation/DesktopMenu.tsx
+++ src/components/navigation/DesktopMenu.tsx
@@ -1,23 +1,23 @@
-"use client";
-
 import MenuLink from "./MenuLink";
 
 import { css } from "../../../styled-system/css";
 import { hstack } from "../../../styled-system/patterns";
 
-import type { MenuItem } from "@typesDef/general";
+import type { INavigationContent } from "@app/layout";
 
-interface DeskTopMenuProps {}
+interface DeskTopMenuProps {
+  navigationLinks: INavigationContent;
+}
 
-export default function DesktopMenu({}: DeskTopMenuProps) {
-  const menuContent: MenuItem[] = [
-    { name: "HOME", src: "/" },
-    { name: "PORTFOLIO", src: "/portfolio" },
-    { name: "CONTACT", src: "/contact" },
-  ];
-
-  const menuItems = menuContent.map((item) => {
-    return <MenuLink key={item.name} href={item.src} label={item.name} />;
+export default function DesktopMenu({ navigationLinks }: DeskTopMenuProps) {
+  const menuItems = navigationLinks.navigationLinks.map((item) => {
+    return (
+      <MenuLink
+        key={item._modelApiKey || ""}
+        href={item.linkUrl || ""}
+        label={item.linkName || ""}
+      />
+    );
   });
 
   return (

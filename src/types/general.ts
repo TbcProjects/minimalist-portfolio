3df503c8888c9ddf91324d518807bf2f6fe64830
TbcProjects <daniel@thebikeclub.co.uk>
2023-07-12 16:33:21 +0100

    Add SocialIcons enum to general types

--- src/types/general.ts
+++ src/types/general.ts
@@ -2,3 +2,9 @@ export type MenuItem = {
   name: string;
   src: string;
 };
+
+export enum SocialIcon {
+  TWITTER_ICON = "twitter_icon",
+  GITHUB_ICON = "github_icon",
+  LINKEDIN_ICON = "linkedin_icon",
+}

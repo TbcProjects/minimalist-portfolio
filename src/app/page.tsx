3b564ca820b291045fab6943ce9e784cafee05c5
TbcProjects <daniel@thebikeclub.co.uk>
2023-07-12 13:06:17 +0100

    Remove un-used imports from index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,14 @@
-import { gql } from "graphql-request";
-
-import { datoClient } from "@lib/datocms/client";
-
-import { css } from "../../styled-system/css";
-
 // section components
 import Hero from "@components/sections/Hero";
 import AboutMe from "@components/sections/AboutMe";
 import ContactBanner from "@components/sections/ContactBanner";
-/**
- * @todo - add head component
- */
 
 export default async function Home() {
-  // const data = await datoClient(query);
-
   return (
-    <>
+    <main>
       <Hero />
       <AboutMe />
       <ContactBanner />
-    </>
+    </main>
   );
 }

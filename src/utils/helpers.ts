fb990cfaadb0f0194a63cb522246411c9923df08
TbcProjects <daniel@thebikeclub.co.uk>
2023-07-13 13:11:05 +0100

    WIP function to find content by key

---
+++ src/utils/helpers.ts
@@ -0,0 +1,10 @@
+// import  { PageContentData } from "@typesDef/general";
+
+// import { IHomepageData } from "@app/page";
+// import { ISiteLayoutData } from "@app/layout";
+
+// const findContentByKey = (content: PageContentData, key: string) => {
+//   return content?.find(
+//     (item) => item._modelApiKey && item._modelApiKey === key
+//   );
+// };

7a97d21fb8bc92bbf9cd31606ce62734ab142c0d
TbcProjects <daniel@thebikeclub.co.uk>
2023-07-13 10:20:00 +0100

    Build Hero design for desktop only

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -3,26 +3,88 @@ import Image from "next/image";
 import { css } from "../../../styled-system/css";
 import { container } from "../../../styled-system/patterns";
 
+import ButtonLink from "@components/shared/button/ButtonLink";
+
+import { IHeroContent } from "@app/page";
+
 interface HeroProps {
-  data?: any;
+  content: IHeroContent;
 }
 
-export default function Hero({ data }: HeroProps) {
+export default function Hero({ content }: HeroProps) {
+  const { heroHeadline, heroImage, buttonLabel, buttonLink } = content;
+
   return (
     <div
       className={container({
         maxW: "breakpoint-lg",
         px: { base: "32px", md: "38px", lg: "0" },
+        pb: { md: "150px" },
       })}
     >
-      <div className={css({ pos: "relative", minH: "552px", width: "100%" })}>
-        <Image
-          src={"/images/jk-placeholder-image.jpeg"}
-          alt="placeholder image"
-          objectFit="cover"
-          fill={true}
-        />
+      <div
+        className={css({
+          position: "relative",
+          maxH: "600px",
+        })}
+      >
+        <div
+          className={css({
+            pos: "relative",
+            minH: { md: "600px" },
+            width: "100%",
+          })}
+        >
+          {heroImage?.url && heroImage.alt && (
+            <Image
+              src={heroImage.url}
+              alt={heroImage.alt}
+              objectFit="cover"
+              fill={true}
+            />
+          )}
+        </div>
+        <div
+          className={css({
+            position: "relative",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "end",
+            alignItems: "flex-start",
+            bottom: "357px",
+            background: "white",
+            height: "357px",
+            w: "40%",
+            paddingRight: "55px",
+          })}
+        >
+          {heroHeadline && (
+            <h1
+              className={css({
+                textStyle: "h1",
+                marginBottom: "53px",
+              })}
+            >
+              {heroHeadline}
+            </h1>
+          )}
+          {buttonLink && buttonLabel && (
+            <ButtonLink variant="primary" href={buttonLink}>
+              {buttonLabel}
+            </ButtonLink>
+          )}
+        </div>
       </div>
+      <div
+        className={css({
+          border: "1px solid black",
+          width: "0",
+          height: "600px",
+          position: "absolute",
+          top: "0",
+          left: "-69px",
+        })}
+      ></div>
     </div>
   );
 }

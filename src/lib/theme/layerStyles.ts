260a3f130868f5c89db51fe28b0877a4e6fbe46b
TbcProjects <daniel@thebikeclub.co.uk>
2023-07-15 00:15:38 +0100

    Remove border none + format

--- src/lib/theme/layerStyles.ts
+++ src/lib/theme/layerStyles.ts
@@ -1,41 +1,40 @@
 // @ts-nocheck
-import { defineLayerStyles } from '@pandacss/dev';
+import { defineLayerStyles } from "@pandacss/dev";
 
 export const layerStyles: LayerStyle = defineLayerStyles({
   primaryButton: {
     value: {
-      color: '#FFF',
-      background: 'japanese_indigo',
-      '& span': {
-        background: 'yankee_blue',
+      color: "#FFF",
+      background: "japanese_indigo",
+      "& span": {
+        background: "yankee_blue",
       },
       _hover: {
-        background: 'cyan',
-        '& span': {
-          background: 'pine',
+        background: "cyan",
+        "& span": {
+          background: "pine",
         },
-        '& svg path': {
-          stroke: 'white',
+        "& svg path": {
+          stroke: "white",
         },
       },
     },
   },
   secondaryButton: {
     value: {
-      color: 'black',
-      background: 'white',
-      border: '1px solid #33323D',
+      color: "black",
+      background: "white",
+      border: "1px solid #33323D",
       _hover: {
-        color: 'white',
-        background: 'greyDarkBlue',
-        border: 'none',
+        color: "white",
+        background: "greyDarkBlue",
       },
     },
   },
   darkBlueOnWhite: {
     value: {
-      background: 'greyDarkBlue',
-      color: 'white',
+      background: "greyDarkBlue",
+      color: "white",
     },
   },
 });

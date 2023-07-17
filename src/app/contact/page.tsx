1c80e79b9b582f01f0cc52260a62a5eeb0d40108
TbcProjects <daniel@thebikeclub.co.uk>
2023-07-17 23:14:46 +0100

    Create types + fetch content page data

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,45 @@
-import Link from 'next/link'
+import { fetchContactpageData } from "@utils/fetchDato";
 
-export default function ContactPage() {
-  return <div>
-    <h1>Contact page</h1>
-    <Link href='/'>Back to home</Link>
-  </div>;
+import { IContactpageQuery } from "@typesDef/dato";
+
+// sections
+import ContactDetail from "@components/sections/ContactDetail";
+
+type IContactpageData = NonNullable<
+  IContactpageQuery["contactpage"]
+>["contactpageContent"];
+
+export type IContactpageContent = {
+  [K in NonNullable<
+    IContactpageData[number]["__typename"]
+  >]: IContactpageData[number] & {
+    __typename: K;
+  };
+};
+
+export type IContactDetailContent =
+  IContactpageContent["SectionContactDetailRecord"];
+
+const findContentByKey = (content: IContactpageData, key: string) => {
+  return content?.find(
+    (item) => item._modelApiKey && item._modelApiKey === key
+  );
+};
+
+export default async function ContactPage() {
+  const data = await fetchContactpageData();
+  const contactpageData = data.contactpage
+    ?.contactpageContent as IContactpageData;
+
+  const contactDetailContent = findContentByKey(
+    contactpageData,
+    "section_contact_detail"
+  );
+
+  return (
+    <main>
+      <ContactDetail />
+      <pre>{JSON.stringify(contactDetailContent, null, 2)}</pre>
+    </main>
+  );
 }
